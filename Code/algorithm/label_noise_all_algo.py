fb3c918988cd87af892206da0245b79009556c94
chen chen <chen.chen.adl@gmail.com>
2018-10-19 11:49:51 +1100

    all algorithms

--- Code/algorithm/label_noise_all_algo.py
+++ Code/algorithm/label_noise_all_algo.py
@@ -25,9 +25,10 @@ from os import cpu_count
 from sklearn.decomposition import IncrementalPCA as PCA
 from sklearn.model_selection import train_test_split
 from multiprocessing import Pool
-#from random import sample
-
-
+import time
+from scipy.spatial.distance import cdist
+from scipy import exp
+from itertools import product
 #
 plot=0
 
@@ -106,8 +107,41 @@ def estimateBeta(S,prob,rho0,rho1):
 
 
 
-import time
+def my_kernel(X, Y):
+    """
+    We create a custom kernel:
+        should give 87% if it matches the original RBF kernel
+    """
+    S=0.84#parameter from rhos
+    
+    if dset==1:
+        gamma= 0.0005   
+    else:
+        gamma= 0.00087 # maximise variance of kernel matrix        
+    if np.array_equal(X,Y):
+        N = X.shape[0]
+        M=(1-S)*np.ones((N,N))+S*np.eye(N)
+    else:
+        M=1
+    
+    pairwise_sq_dists = cdist(X, Y, 'sqeuclidean')
+    K = exp(-gamma*pairwise_sq_dists)*M
+    return K
+
 # dset chooses dataset. num_run determines the number of iterations.
+def expectationMaximisation(run):    
+    np.random.seed((run**5+1323002)%123123)#np.random.seed() alternatively
+    print("dset:",dset,'run',run)
+    Xtr,Str,Xts,Yts = data_cache[dset]
+    X_train, X_val, y_train, y_val = train_test_split(Xtr, Str, test_size=0.2)
+    #clf1 is the first classifier while clf2 is the second
+    clf = svm.SVC(C=2.5,kernel=my_kernel)
+    if run==1:
+        print("learn initial probability dset:",dset)  
+    clf.fit(X_train,y_train)
+
+    return clf.score(Xts,Yts)
+    #23:08 23:12 23:28 4.2577 
 def cv_reweighting(run):    
     np.random.seed((run**5+1323002)%123123)#np.random.seed() alternatively
     print("dset:",dset,'run',run)
@@ -118,7 +152,7 @@ def cv_reweighting(run):
         clf1 = svm.SVC(C=2.5,gamma=0.000225,probability=True)
     else:
         #removed 'gamma=scale'. should be the default.
-        clf1 = svm.SVC(probability=True,gamma=0.00865)
+        clf1 = svm.SVC(probability=True,gamma='scale')
     if run==1:
         print("learn initial probability dset:",dset)        
     clf1.fit(X_train,y_train)
@@ -147,13 +181,65 @@ def cv_reweighting(run):
     # 85.5 for dataset 2.
     return clf2.score(Xts,Yts)
     #23:08 23:12 23:28 4.2577 
+    
+def relabelling(run):    
+    np.random.seed((run**5+1323002)%123123)#np.random.seed() alternatively
+    print("dset:",dset,'run',run)
+    Xtr,Str,Xts,Yts = data_cache[dset]
+    X_train, X_val, y_train, y_val = train_test_split(Xtr, Str, test_size=0.2)
+    #clf1 is the first classifier while clf2 is the second
+    if dset==2:
+        clf1 = svm.SVC(C=2.5,gamma=0.000225,probability=True)
+    else:
+        clf1 = svm.SVC(gamma='scale',probability=True)
+    if run==1:
+        print("learn initial probability dset:",dset)    
+    clf1.fit(X_train,y_train)   
+    if run==1:
+        print("calculating weighting dset:",dset)
+    bb = clf1.predict_proba(X_train)
+    print("estimate of [rho0 rho1]=",np.amin(bb, axis=0)) #rho0 rho1
+    nn=len(y_train)
+    ind=np.where(abs(bb[:,1]-y_train)>=0.5)
+    y_train[ind]=1-y_train[ind]    
+    ind_p=int(nn/3)
+    ind5=np.hstack((np.argsort(-bb[:,1])[0:ind_p],np.argsort(-bb[:,0])[0:ind_p]))
+    
+    #The best parameters are {'C': 2.1544346900318834, 'gamma': 0.01} with a score of 1.00
+#    search=0 #search for parameters
+#    if search:
+#        C_range = 1
+#        gamma_range = np.logspace(10**-4,10**1, 4)
+#        param_grid = dict(gamma=gamma_range, C=C_range)
+#        cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2)
+#        grid = GridSearchCV(svm.SVC(), param_grid=param_grid, cv=cv,n_jobs=-1)
+#        grid.fit(Xtr[ind5,:],Str[ind5])
+#        print("The best parameters are %s with a score of %0.2f"
+#              % (grid.best_params_, grid.best_score_))    
+    if dset==2:
+        clf2 = svm.SVC(gamma=0.000225)
+    else:
+        clf2 = svm.SVC(gamma=0.00865)
+    clf2.fit(X_train[ind5,:],y_train[ind5])
+    return clf2.score(Xts,Yts)
+    #23:08 23:12 23:28 4.2577 
+    
 def run_algorithm(alg_type, dset, num_run):   #alg_type: type of the algorithm, choose from 'reweighting',...tbc
     start=time.time()
     print('start of the whole algorithm with dataset',dset)
+    pool = Pool(processes=cpu_count())
     if alg_type=='reweighting':
-        print('start of reweighting algorithm')
-        pool = Pool(processes=cpu_count())
+        print('start of reweighting algorithm')        
         it = pool.map(cv_reweighting, range(num_run))  #using the number of runs
+        #test_score=np.zeros(num_run)
+        #for i in range(num_run):
+            #test_score[i]=cv_reweighting(1)
+    if alg_type=='relabelling':
+        print('start of relabelling algorithm')
+        it = pool.map(relabelling, range(num_run))  #using the number of runs
+    if alg_type=='expectationMaximisation':
+        print('start of relabelling algorithm')
+        it = pool.map(expectationMaximisation, range(num_run))  #using the number of runs
         #test_score=np.zeros(num_run)
         #for i in range(num_run):
             #test_score[i]=cv_reweighting(1)
@@ -164,15 +250,25 @@ def run_algorithm(alg_type, dset, num_run):   #alg_type: type of the algorithm, 
     std_score=np.std(test_score)
     print('average score: ',average_score,'\nstandard deviation: ',std_score) # help to format here!
     end=time.time()
-    with open('result'+'_data'+str(dset)+'_'+alg_type+'.txt', 'w') as f: #better way to output result? I would like they can be read into python easily
+    with open('result'+'_data'+str(dset)+'_'+alg_type+str(round(end-start,4))+'sec.txt', 'w') as f: #better way to output result? I would like they can be read into python easily
         for item in test_score:
             f.write("%s\n" % item)
     
     print('total process time is',round(end-start,4),'sec')
     
     return average_score, std_score
-#change it to for loop?
-dset=1
-average_score1, std_score1 = run_algorithm('reweighting',dset,16)
-dset=2
-average_score2, std_score2 = run_algorithm('reweighting',dset,16)
+
+average_score = {}
+std_score={}
+#TODO!please change it to for loop! for dset,algo in range(2),algos ...
+for dset, algo in product([1,2],['reweighting','relabelling','expectationMaximisation']):
+    ind='dataset '+str(dset)+' '+algo
+    average_score[ind],std_score[ind]=run_algorithm(algo,dset,16)
+#dset=1
+#average_score11, std_score11 = run_algorithm('reweighting',dset,16)
+#average_score12, std_score12 = run_algorithm('relabelling',dset,16)
+#average_score13, std_score13 = run_algorithm('expectationMaximisation',dset,16)
+#dset=2
+#average_score21, std_score21 = run_algorithm('reweighting',dset,16)
+#average_score22, std_score22 = run_algorithm('relabelling',dset,16)
+#average_score23, std_score23 = run_algorithm('expectationMaximisation',dset,16)
