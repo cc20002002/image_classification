f744ea6cfc354b109658e4ddd7fa5fbfd83f7bcf
chen chen <chen.chen.adl@gmail.com>
2018-10-31 20:01:43 +1100

    plots

--- Code/algorithm/plots.R
+++ Code/algorithm/plots.R
@@ -1,6 +1,12 @@
+setwd("C:/Users/chenc/Documents/GitHub/image_classification/code/algorithm")
 
 temp = list.files(pattern="*.csv")
-temps = list.files(pattern="result_data")
+setwd("C:/Users/chenc/Documents/GitHub/image_classification/code/algorithm/nopca")
+
+temps = list.files(pattern="*.csv")
+myfiless = lapply(temps, read.csv,header=FALSE)
+myfiless=do.call(rbind,myfiless)
+setwd("C:/Users/chenc/Documents/GitHub/image_classification/code/algorithm")
 temp=setdiff(temp,temps)
 temp=temp[1:54]
 temp2=gsub(pattern = 'sec.csv',replacement = '',x=temp)
@@ -8,7 +14,7 @@ temp22=gsub(pattern = 'sec.csv',replacement = '',x=temps)
 
 require('stringr')
 temp2=str_sub(temp2, start = 20, end = -1L)
-#temp22=str_sub(temp22, start = 20, end = -1L)
+temp22=str_sub(temp22, start = 20, end = -1L)
 temp3=as.numeric(gsub("[^0-9.]", "", temp2) )/16
 temp32=as.numeric(gsub("[^0-9.]", "", temp22) )/16
 myfiles = lapply(temp, read.csv,header=FALSE)
@@ -22,16 +28,19 @@ for (i in 1:6){
 myfiles$data_size[seq(i,54,by=6)]=data
 }
 #############
-myfiless = lapply(temps, read.csv,header=FALSE)
-myfiless=do.call(rbind,myfiless)
+
+
 myfiless$data_size=1
-myfiless$algorithm=c(rep('Expectation Maximisation',9),'Relabelling','Importance Reweighting')
-myfiless$dataset=c('CIFAR without PCA','CIFAR without PCA','CIFAR without PCA')
+myfiless$algorithm=c('Expectation Maximisation','Relabelling','Importance Reweighting')
+myfiless$dataset='CIFAR without PCA'
 myfiless$`Running time`=temp32
 data=seq(10000,2000,by=-1000)
+
 for (i in 1:3){
   myfiless$data_size[seq(i,27,by=3)]=data
 }
+myfiles=rbind(myfiles,myfiless)
+#with(myfiless,order(dataset,algorithm))
 require('data.table')
 library(scales) 
 #################
@@ -39,6 +48,7 @@ require(ggplot2)
 require(dplyr)
 df1=melt(myfiles, id=17:20)
 df1=df1[,c(1,2,3,4,6)]
+lm(data=myfiless[seq(1,27,by=3),],formula=log(`Running time`)~log(data_size))
 names(df1)=c( "Sample size", "Algorithm", "Data set",  'Average running time (seconds)', "Accuracy"   )
 ggplot(df1, aes(x=`Sample size`, y=`Average running time (seconds)`,linetype=`Data set`,colour=Algorithm,fill=`Data set`)) +
 geom_point(aes(shape=`Data set`),size=3)+
@@ -49,10 +59,12 @@ geom_point(aes(shape=`Data set`),size=3)+
     labels = scales::trans_format("log10", scales::math_format(10^.x))
   ) +geom_smooth(method='lm',se=F)
 ggsave(filename = 'speed.pdf',width = 7, height = 4, units = "in")
+
 ggplot(df1, aes(x=`Sample size`, y=`Accuracy`,colour=Algorithm,fill=`Data set`,linetype=`Data set`)) +
 geom_point(aes(shape=`Data set`),size=3) +
 geom_smooth(alpha=0.5)
 ggsave(filename = 'accuracy.pdf',width = 7, height = 4.5, units = "in")
+
 
 
 tempd=df1%>%filter(`Sample size`==4000)
